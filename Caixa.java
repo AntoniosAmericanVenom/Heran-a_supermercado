63778d710536d936067b89aebe3ce50d3a244e62
AntonioBflores <totonhofloresss@gmail.com>
2026-06-29 19:21:53 -0300

    Add files via upload

---
+++ Caixa.java
@@ -0,0 +1,22 @@
+public class Caixa extends Funcionario {
+private int salarioCaixa;
+
+
+
+public Caixa(int salarioCaixa) {
+    super();
+    this.salarioCaixa = salarioCaixa;
+}
+
+public Caixa() {
+}
+
+public int getSalarioCaixa() {
+    return salarioCaixa;
+}
+
+public void setSalarioCaixa(int salarioCaixa) {
+    this.salarioCaixa = salarioCaixa;
+}  
+
+}
---
+++ Repositor.java
@@ -0,0 +1,19 @@
+public class Repositor extends Funcionario {
+private int salarioRepositor;
+
+
+
+public Repositor(int salarioRepositor) {
+    super();
+    this.salarioRepositor = salarioRepositor;
+}
+
+public int getSalarioRepositor() {
+    return salarioRepositor;
+}
+
+public void setSalarioRepositor(int salarioRepositor) {
+    this.salarioRepositor = salarioRepositor;
+}
+
+}
